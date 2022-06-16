b1181e83cbd351cdc978326da427d45ebf75b5c8
AxelGamma <axelhgbryan@gmail.com>
2022-06-15 20:22:31 -0500

    Update

--- Triggers.sql
+++ Triggers.sql
@@ -60,6 +60,76 @@ update docente set rfc='FAN43LS0LRMN' where nombre='Fausto Antonio';
 select * from docente;
 select * from docente order by id_docente;
 
+--Otro ejemplos
+create or replace function fun_rfc()
+    returns trigger as
+$$
+BEGIN
+    --Old es una variable que va tener una informacion antigua de la tabla
+    --New es la variable que va contener la nueva fila de la tabla como insert o como updates
+    if length(new.rfc) > 13 or new.rfc is null or length(new.rfc) < 13 then
+        raise exception 'El rfc tiene que contener 13 digitos.';
+    end if;
+
+    --Si insertamos un valor nullo nos va impedir ingresar ese dato
+    if (new.id_edificio) is null then
+        raise exception 'No se aceptan nulos';
+    end if;
+    if (new.nombre) ='' then
+        raise exception 'No se aceptan nulos o espacios en blanco';
+    end if;
+    if( new.apellidos) ='' then
+        raise exception 'No se aceptan nulos o espacios en blanco';
+    end if;
+    if (new.rfc) ='' then
+        raise exception 'No se aceptan nulos o espacios en blanco';
+    end if;
+    if (new.telefono) is null then
+        raise exception 'No se aceptan nulos ';
+    end if;
+    if (new.cedula_docente) ='' then
+        raise exception 'No se aceptan nulos o espacios en blanco';
+    end if;
+    if (new.puesto)=''  then
+        raise exception 'No se aceptan nulos';
+    end if;
+    if (new.email_institucional) ='' then
+        raise exception 'No se aceptan nulos';
+    end if;
+    return new;
+end;
+$$ language plpgsql;
+
+--Creacion de trigger
+create trigger error_nulos
+    before insert or update
+    on docente
+    for each row
+execute function fun_rfc();
+
+drop trigger IF EXISTS error_nulos on docente;
+
+insert into docente
+values (default, 2, 'Alberto Isacc', 'Guzman Meza', '1234567890123', 833294032, 'LKSIO345SL',
+        '', 'quimica@cdmadero.tecnm.com');
+
+
+select *
+from docente;
+
+CREATE or replace FUNCTION suma(x integer, y integer) RETURNS integer AS
+$$
+begin
+     return x + y ;
+end;
+$$ LANGUAGE plpgsql;
+
+
+SELECT suma(23231, 623);
+
+
+
+
 
 
 
